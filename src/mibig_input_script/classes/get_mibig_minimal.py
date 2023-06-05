2abffe5b5e52e93041694ae30af52fa79e83a712
Mitja Zdouc <mitja.zdouc@wur.nl>
2023-06-05 12:57:12 +0200

    Added biosynthetic class support

--- src/mibig_input_script/classes/get_mibig_minimal.py
+++ src/mibig_input_script/classes/get_mibig_minimal.py
@@ -6,7 +6,7 @@
 
 import argparse
 from pathlib import Path
-from typing import Dict, Self
+from typing import Dict, Self, List
 
 
 class Minimal:
@@ -18,25 +18,22 @@ class Minimal:
 
     def __init__(self: Self):
         self.existing_entry = None
-        self.biosynth_class = None
-        self.compound = None
-        self.accession = None
+        self.biosynth_class: List[str] = []
+        self.compound = None  #
+        self.accession = None  #
         self.completeness = None
-        self.start_coord = None
-        self.end_coord = None
-        self.publications = None
+        self.start_coord = None  #
+        self.end_coord = None  #
+        self.publications = None  #
         self.mibig_accession = None
         self.organism_name = None
         self.ncbi_tax_id = None
         self.minimal = None
         self.status = None
-
-        # need to by try/except
-        self.chem_struct = None
-        self.evidence = None
+        self.evidence = None  #
 
     def load_existing(self: Self, existing: Dict) -> None:
-        """Load and store an existing MIBiG entry for manipulation
+        """Load the data ofa nexisting entry for later manipulation.
 
         Parameters:
             `self` : The instance of class Minimal.
@@ -57,23 +54,18 @@ class Minimal:
         self.mibig_accession = existing["cluster"]["mibig_accession"]
         self.organism_name = existing["cluster"]["organism_name"]
         self.ncbi_tax_id = existing["cluster"]["ncbi_tax_id"]
-        self.minimal = existing["cluster"]["minimal"]
-        self.status = existing["cluster"]["status"]
+        self.minimal = existing["cluster"]["minimal"]  # not needed?
+        self.status = existing["cluster"]["status"]  # not needed?
 
         try:
-            self.chem_struct = existing["cluster"]["compounds"][0]["chem_struct"]
-        except KeyError:
-            self.chem_struct = None
-
-        try:
-            self.evidence = existing["cluster"]["compounds"][0]["evidence"]
+            self.evidence = existing["cluster"]["loci"]["evidence"]
         except KeyError:
             self.evidence = None
 
     def get_new_mibig_accession(
         self: Self, args: argparse.Namespace, ROOT: Path
     ) -> None:
-        """Create a MIBiG accession number for the new entry.
+        """Generate a non-existing temporary MIBiG ID for the new entry.
 
         Parameters:
             `self` : The instance of class Minimal.
@@ -105,45 +97,111 @@ class Minimal:
         """
         if self.existing_entry is not None:
             message = (
-                f"===========================================\n"
+                f"================================================\n"
                 f"You are MODIFYING the existing MIBiG entry:\n"
-                f"{self.mibig_accession}\n"
-                f"Press 'Ctrl+D' to cancel without saving.\n"
-                f"===========================================\n"
+                f"{self.mibig_accession}"
             )
             print(message)
         else:
             message = (
-                f"==============================================\n"
+                f"================================================\n"
                 f"You are CREATING a new MIBiG entry with the ID\n"
-                f"{self.mibig_accession}\n"
-                f"Press 'Ctrl+D' to cancel without saving.\n"
-                f"==============================================\n"
+                f"{self.mibig_accession}"
             )
             print(message)
 
-        # ~ while True:
+        start_message = (
+            "================================================\n"
+            "This menu allows to modify the minimum infor-\n"
+            "mation of a MIBiG entry. To modify information,\n"
+            "type its corresponding number and press enter.\n"
+            "Press 'Ctrl+D' to cancel without saving.\n"
+            "================================================"
+        )
+        print(start_message)
 
-        # print options menu (which entry to add/change)
-        # this then calls a separate function with input field and input testing and variable assignment
-        # then goes back to top until
+        options = {"1": self.get_biosynth_class}
 
-        # START WHILE LOOP HERE!
+        while True:
+            input_message = (
+                f"0) Save and continue\n"
+                f"1) Biosynthetic class(es) (currently: {self.biosynth_class})\n"
+                f"2) Compound name(s) (currently: {self.compound})\n"
+                f"3) NCBI accession number (currently: {self.accession})\n"
+                f"4) BGC start/end coordinates (currently: {self.start_coord}:{self.end_coord})\n"
+                f"5) BGC evidence (currently: {self.evidence})\n"
+                f"6) Publication/reference (currently: {self.publications})\n"
+                f"================================================\n"
+            )
+            user_input = input(input_message)
+
+            if user_input == "0":
+                # function to check if all necessary values are there and to add missing ones
+                break
+            elif user_input in options:
+                options[user_input]()
+            else:
+                error_message = (
+                    "================================================\n"
+                    "Please enter a valid number.\n"
+                    "================================================\n"
+                )
+                print(error_message)
+
+    def get_biosynth_class(self: Self) -> None:
+        """Get the biosynthetic class of BGC and test if valid
+
+        Parameters:
+            `self` : The instance of class Minimal.
+
+        Returns:
+            None
+        """
+        allowed_bgc = [
+            "Alkaloid",
+            "Polyketide",
+            "RiPP",
+            "NRP",
+            "Saccharide",
+            "Terpene",
+            "Other",
+        ]
+        error_message = (
+            "================================================\n"
+            "Please enter a valid BGC.\n"
+            "================================================\n"
+        )
+
+        input_message = (
+            f"Type in the biosynthetic class of the BGC.\n"
+            f"To specify multiple classes, separate entries with whitespace.\n"
+            f"Allowed entries are:\n"
+            f"------------------------------------------------\n"
+            f"{' '.join([i for i in allowed_bgc]) }\n"
+            f"------------------------------------------------\n"
+        )
+        input_raw = input(input_message)
+        user_input = input_raw.split()
+
+        if not len(user_input) == 0:
+            self.biosynth_class = set()
+            for i in user_input:
+                if i in allowed_bgc:
+                    self.biosynth_class.add(i)
+                else:
+                    print(error_message)
+                    return
+            self.biosynth_class = list(self.biosynth_class)
+            return
+        else:
+            print(error_message)
+            return
 
         # Set a flag for "modified" -> write new entry
         # Else, skip writing - abort
 
-    def test_placeholder(self: Self, input_value: str) -> bool:
-        """Perform test on input value
-
-        Parameters:
-            `self` : The instance of class Minimal.
-            `input_value` : The input value to test.
-
-        Returns:
-            A `bool` to indicate outcome of test
-        """
-        pass
+    # funciton idea: while loop for both reading and user input the same -> writes to same attributes
+    # esearch function for taxid and organism name (via BioPython)
 
     def create_dict(self: Self) -> Dict:
         """Summarize values in json-compatible dict
@@ -155,12 +213,6 @@ class Minimal:
             A `dict` to store in MibigEntry() class
         """
         pass
-
-    # funciton idea: while loop for both reading and user input the same -> writes to same attributes
-    # esearch function for taxid and organism name (via BioPython)
-
-
-# redo this function - already read
 
 
 def get_mibig_minimal(
